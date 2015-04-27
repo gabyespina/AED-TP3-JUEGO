f470731d985deaf6066cfd1be9fdb5db6feda10a
Gabriel <gabyespina.ge@yahoo.com>
2015-04-27 17:22:52 -0300

    Update Juego.cpp

--- Juego.cpp
+++ Juego.cpp
@@ -1,11 +1,9 @@
-//TODAVIA NO TERMINÉ EL PROGRAMA
-
 #include <cstdlib>
 #include <iostream>
 
 using namespace std;
 
-int crear_preguntas(int);
+int preguntas(int);
 
 int main(int argc, char *argv[])
 {   
@@ -14,20 +12,30 @@ int main(int argc, char *argv[])
     cout<<" Elija el tipo de pregunta : "<<endl<<endl;
     cout<<"   Matematica : 1"<<endl
         <<"   Geografia  : 2"<<endl
-        <<"   Historia   : 3"<<endl<<endl<<"   ";
+        <<"   Historia   : 3"<<endl
+        <<" -------------------"<<endl
+        <<"   Salir      : 4"<<endl<<endl<<"   ";
 
     int tipo; cin>>tipo;
     
-    
-   /* for (pos= 0; pos<10;pos++){
-        tipo[pos]= pos;
-        cout<< tipo[pos]<< "  ,  ";
-    }
-   */
+   switch (tipo){
+       case 1:
+            cout<<"
+            break;   
+       
+       case 2 :
+            break; 
+       
+       case 3 :
+            break;
+       
+       case 4 :
+            cout<<endl<<" FIN DEL JUEGO, HASTA LUEGO"<<endl<<endl;
+            break;
+       default :
+               cout<<"Item Incorrecto" ;
+               break;  
+   }
     system("PAUSE");
 }
 
-int crear_preguntas(int tipo){
-    tipo = tipo + 3;
-    return tipo;
-}
